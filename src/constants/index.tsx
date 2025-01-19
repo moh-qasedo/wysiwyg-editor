3abca44b3b67f1193c7366be29b0a7b5e28b3e29
Mohammed Qasedo <qasedo2000@gmail.com>
2025-01-19 10:21:58 +0200

    add constants module

---
+++ src/constants/index.tsx
@@ -0,0 +1,26 @@
+import boldImg from "../assets/images/bold.png";
+import italicImg from "../assets/images/italic.png";
+import underlineImg from "../assets/images/underline.png";
+import codeImg from "../assets/images/code.png";
+import saveImg from "../assets/images/save.png";
+
+const CONSTANTS = {
+  DEFAULT_CLASSNAMES: {
+    CONTAINER: "container",
+    TEXT: "text",
+    IMAGE: "img",
+    BUTTON: "button",
+    SELECTED: "selected",
+    TOOLBAR: "toolbar",
+    EDITOR: "editor",
+  },
+  IMAGES: {
+    BOLD: boldImg,
+    ITALIC: italicImg,
+    UNDERLINE: underlineImg,
+    CODE: codeImg,
+    SAVE: saveImg,
+  },
+};
+
+export default CONSTANTS;
